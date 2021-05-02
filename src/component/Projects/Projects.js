dae68ac1273a4cadb0d1a3098c17dfd917455d50
Minhaj Sadik <MinhajoSadik@gmail.com>
2021-05-02 20:10:51 +0600

    resume profile done

--- src/component/Projects/Projects.js
+++ src/component/Projects/Projects.js
@@ -12,27 +12,27 @@ const Projects = () => {
     const allProjects = [
         {
             id: '01', img: "" + creative, title: "Creative Agency", description: "A fullstack creative agency website.",
-            gitHub: "https://github.com/MinhajSadik", liveLink: "https://creative-agency-200b7.web.app/"
+            gitHub: "https://github.com/MinhajSadik/Creative-Agency", liveLink: "https://creative-agency-200b7.web.app/"
         },
 
         {
             id: '02', img: "" + volunteer, title: "Volunteer-Network", description: "A fullstack hot volunteer network  website.",
-            gitHub: "https://github.com/MinhajSadik", liveLink: "https://volunteer-network-5b947.web.app/"
+            gitHub: "https://github.com/MinhajSadik/Volunteer-Network", liveLink: "https://volunteer-network-5b947.web.app/"
         },
 
         {
             id: '03', img: "" + travel, title: "Travel Guru", description: "A fullstack travel guru website.",
-            gitHub: "https://github.com/MinhajSadik", liveLink: "https://distracted-varahamihira-1c0a00.netlify.app/"
+            gitHub: "https://github.com/MinhajSadik/Travel-Guru", liveLink: "https://distracted-varahamihira-1c0a00.netlify.app/"
         },
 
         {
             id: '04', img: "" + hotGadgets, title: "Hot Gatgets", description: "A front-end E-commerce website.",
-            gitHub: "https://github.com/MinhajSadik", liveLink: "https://rabbism.github.io/hot-gadget/"
+            gitHub: "https://github.com/MinhajSadik/Hot-Gadget", liveLink: "https://minhajsadik.github.io/Hot-Gadget/"
         },
 
         {
             id: '05', img: "" + pandaCommerce, title: "Panda Commerce", description: "A front-end Panda Commerce website.",
-            gitHub: "https://github.com/MinhajSadik", liveLink: "https://rabbism.github.io/panda-commerce-site/"
+            gitHub: "https://github.com/MinhajSadik/Panda-E-commerce-Bootstrap", liveLink: "https://minhajsadik.github.io/Panda-E-commerce-Bootstrap/Index.html"
         }
     ]
 

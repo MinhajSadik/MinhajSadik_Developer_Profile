8393467ac7bdbac51b3c7f46cb6e5e8b57552107
Minhaj Sadik <MinhajoSadik@gmail.com>
2021-05-03 15:25:02 +0600

    practical.js working

--- src/component/Intro/Intro.js
+++ src/component/Intro/Intro.js
@@ -5,6 +5,7 @@ import { FaGithubSquare } from 'react-icons/fa';
 import { AiOutlineMedium, AiFillLinkedin, AiFillYoutube } from 'react-icons/ai';
 import CV from '../../images/resume.pdf';
 import { Link } from 'react-router-dom';
+import Particles from 'react-particles-js';
 
 
 
@@ -24,6 +25,71 @@ const Intro = () => {
                 </h3>
                 <a href={CV} download="Resume"><button className="btn btn-success my-2 my-sm-0 btn-style">Get Resume</button></a>
                 <Link to="/about-section"><button className="btn btn-outline-success my-2 my-sm-0 btn-style">About Me</button></Link>
+                <Particles
+                    params={{
+                        "fps_limit": 28,
+                        "particles": {
+                            "collisions": {
+                                "enable": false
+                            },
+                            "number": {
+                                "value": 200,
+                                "density": {
+                                    "enable": false
+                                }
+                            },
+                            "line_linked": {
+                                "enable": true,
+                                "distance": 30,
+                                "opacity": 0.4
+                            },
+                            "move": {
+                                "speed": 1
+                            },
+                            "opacity": {
+                                "anim": {
+                                    "enable": true,
+                                    "opacity_min": 0.05,
+                                    "speed": 1,
+                                    "sync": false
+                                },
+                                "value": 0.4
+                            }
+                        },
+                        "polygon": {
+                            "enable": true,
+                            "scale": 0.5,
+                            "type": "inline",
+                            "move": {
+                                "radius": 10
+                            },
+                            "url": "/small-deer.2a0425af.svg",
+                            "inline": {
+                                "arrangement": "equidistant"
+                            },
+                            "draw": {
+                                "enable": true,
+                                "stroke": {
+                                    "color": "rgba(255, 255, 255, .2)"
+                                }
+                            }
+                        },
+                        "retina_detect": false,
+                        "interactivity": {
+                            "events": {
+                                "onhover": {
+                                    "enable": true,
+                                    "mode": "bubble"
+                                }
+                            },
+                            "modes": {
+                                "bubble": {
+                                    "size": 6,
+                                    "distance": 40
+                                }
+                            }
+                        }
+                    }} />
                 <br />
                 <br />
                 <br />

274d1c83d3394ea971898663ce66cb8245672c7b
Minhaj Sadik <MinhajoSadik@gmail.com>
2021-05-05 06:23:03 +0600

    react-portfolio ready

--- src/component/Projects/Projects.js
+++ src/component/Projects/Projects.js
@@ -1,28 +1,29 @@
 import React from 'react';
 import './Projects.css';
-import creative from '../../images/projectImage/creative.png';
-import travel from '../../images/projectImage/Voluanter.png';
-import volunteer from '../../images/projectImage/travel.png';
+import Grocery from '../../images/projectImage/Grocery-Shop.png';
+import Urban from '../../images/projectImage/Urban-Riders.png';
+import Ema from '../../images/projectImage/Ema-Jhon Simple.png';
 import hotGadgets from '../../images/projectImage/hotGadgets.JPG';
 import pandaCommerce from '../../images/projectImage/pandaCommerce.JPG';
+import Team from '../../images/projectImage/footballTeamSelector.png'
 import Project from './Project';
 
 const Projects = () => {
 
     const allProjects = [
         {
-            id: '01', img: "" + creative, title: "Creative Agency", description: "A fullstack creative agency website.",
-            gitHub: "https://github.com/MinhajSadik/Creative-Agency", liveLink: "https://creative-agency-2007.web.app/"
+            id: '01', img: "" + Grocery, title: "Grocery-Shop", description: "A Full-Stack Grocery-Shop Website.",
+            gitHub: "https://github.com/MinhajSadik/Full-Stack-Grocery-Shop", liveLink: "https://full-stack-grocery-house-shop.netlify.app/"
         },
 
         {
-            id: '02', img: "" + volunteer, title: "Volunteer-Network", description: "A fullstack hot volunteer network  website.",
-            gitHub: "https://github.com/MinhajSadik/Volunteer-Network", liveLink: "https://volunteer-network-5947.web.app/"
+            id: '02', img: "" + Ema, title: "Ema-Jhon Simple", description: "A Full-Stack Ema-Jhon Simple  Website.",
+            gitHub: "https://github.com/MinhajSadik/React-Ema-Jhon-Simple", liveLink: "https://full-stack-ema-jhon-simple.netlify.app/ "
         },
 
         {
-            id: '03', img: "" + travel, title: "Travel Guru", description: "A fullstack travel guru website.",
-            gitHub: "https://github.com/MinhajSadik/Travel-Guru", liveLink: "https://distracted-varahamihira-10a00.netlify.app/"
+            id: '03', img: "" + Urban, title: "Urban Riders", description: "A  Dynamic And  Authorization Website.",
+            gitHub: "https://github.com/MinhajSadik/React-Urban-Riders", liveLink: "https://google-authorization-urban-riders.netlify.app/"
         },
 
         {
@@ -33,6 +34,11 @@ const Projects = () => {
         {
             id: '05', img: "" + pandaCommerce, title: "Panda Commerce", description: "A front-end Panda Commerce website.",
             gitHub: "https://github.com/MinhajSadik/Panda-E-commerce-Bootstrap", liveLink: "https://minhajsadik.github.io/Panda-E-commerce-Bootstrap/Index.html"
+        },
+
+        {
+            id: '05', img: "" + Team, title: "Football Team Selectors", description: "A React-Router Football Team Selectors Websites.",
+            gitHub: "https://github.com/MinhajSadik/Football-League-Router", liveLink: "https://football-league-router.netlify.app/"
         }
     ]
 

3f2533b3276ef7b9009f7c7e1cd5987f5b87dcb7
Minhaj Sadik <MinhajoSadik@gmail.com>
2021-05-07 05:24:14 +0600

    practicls removed

--- src/component/Intro/Intro.js
+++ src/component/Intro/Intro.js
@@ -5,7 +5,6 @@ import { FaGithubSquare } from 'react-icons/fa';
 import { AiOutlineMedium, AiFillLinkedin, AiFillYoutube } from 'react-icons/ai';
 import CV from '../../images/resume.pdf';
 import { Link } from 'react-router-dom';
-import Particles from 'react-particles-js';
 // import Particles from 'react-particles-js'; 
 
 
@@ -23,7 +22,36 @@ const Intro = () => {
                         loop={Infinity}
                         wrapper='b'
                     />
-                    <Particles style={{ width: '50%', height: '50%' }}
+             </h3>
+                <a href={CV} download="Resume"><button className="btn btn-success my-2 my-sm-0 btn-style">Get Resume</button></a>
+                <Link to="/about-section"><button className="btn btn-outline-success my-2 my-sm-0 btn-style">About Me</button></Link>
+                <br />
+                <br />
+                <br />
+                <br />
+
+                <div className="back-line d-flex align-items-center">
+                    <div className="front-box d-flex align-items-center">
+                        <p className="d-flex align-items-center">Follow Me</p>
+                    </div>
+                </div>
+
+                <div className="icon">
+                    <a className="icon2" href="https://github.com/MinhajSadik" target="_blank" rel="noopener noreferrer"><FaGithubSquare /></a>
+                    <a className="icon2" href="https://www.linkedin.com/in/MinhajSadik" target="_blank" rel="noopener noreferrer"><AiFillLinkedin /></a>
+                    <a className="icon2" href="https://medium.com/@minhajsadik" target="_blank" rel="noopener noreferrer"><AiOutlineMedium /></a>
+                    <a className="icon2" href="https://www.youtube.com/channel/UC6I0U9NjTTQQGpKI73_mNXQ" target="_blank" rel="noopener noreferrer"><AiFillYoutube /></a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Intro;
+
+
+// eslint-disable-next-line no-lone-blocks
+{/* <Particles style={{ width: '50%', height: '50%' }}
                         params={{
                             "particles": {
                                 "number": {
@@ -57,30 +85,4 @@ const Intro = () => {
                                     }
                                 }
                             }
-                        }} />
-                </h3>
-                <a href={CV} download="Resume"><button className="btn btn-success my-2 my-sm-0 btn-style">Get Resume</button></a>
-                <Link to="/about-section"><button className="btn btn-outline-success my-2 my-sm-0 btn-style">About Me</button></Link>
-                <br />
-                <br />
-                <br />
-                <br />
-
-                <div className="back-line d-flex align-items-center">
-                    <div className="front-box d-flex align-items-center">
-                        <p className="d-flex align-items-center">Follow Me</p>
-                    </div>
-                </div>
-
-                <div className="icon">
-                    <a className="icon2" href="https://github.com/MinhajSadik" target="_blank" rel="noopener noreferrer"><FaGithubSquare /></a>
-                    <a className="icon2" href="https://www.linkedin.com/in/MinhajSadik" target="_blank" rel="noopener noreferrer"><AiFillLinkedin /></a>
-                    <a className="icon2" href="https://medium.com/@minhajsadik" target="_blank" rel="noopener noreferrer"><AiOutlineMedium /></a>
-                    <a className="icon2" href="https://www.youtube.com/channel/UC6I0U9NjTTQQGpKI73_mNXQ" target="_blank" rel="noopener noreferrer"><AiFillYoutube /></a>
-                </div>
-            </div>
-        </div>
-    );
-};
-
-export default Intro;
+                        }} /> */}

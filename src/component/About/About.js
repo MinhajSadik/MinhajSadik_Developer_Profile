d684358743a7cc473f7bff3cb3448a44cde44fa5
MinhajSadik <MinhajoSadik@gmail.com>
2021-08-29 01:26:33 +0600

    [adding]

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -1,17 +1,22 @@
 /* eslint-disable jsx-a11y/iframe-has-title */
-import React, { useEffect } from 'react';
-import './About.css';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 import Aos from "aos";
 import "aos/dist/aos.css";
-import CV from '../../images/resume.pdf';
-import Minhaj from '../../images/minhaj.jpg';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import Minhaj from '../../images/minhaj.jpg';
+import CV from '../../images/resume.pdf';
+import styles from "./../../styles.module.css";
+import './About.css';
+
 
 
 const About = () => {
     useEffect(() => {
         Aos.init({ duration: 2000 });
     }, [])
+    const alignCenter = { display: 'flex', alignItems: 'center' }
+
 
     return (
         <div id="about-section" className="container about">
@@ -23,15 +28,42 @@ const About = () => {
             <div className="row">
                 <div data-aos="flip-down" className="col-md-6 img-size">
                     <img src={Minhaj} alt="MinhajSadik" />
-                </div>
-                <div data-aos="flip-up" className="col-md-6 about-mySelf">
-                    <div className="iframe">
-                        <iframe style={{ width: '130%', height: '100%' }} src="https://docs.google.com/document/d/e/2PACX-1vTdFQGC_v49mdt6m37odwaYx96zND4RkAU6QJOpQF_QLRD42rYq96sfpBv5YeeoUS5IneQWR4DaQ_RL/pub?embedded=true"></iframe>
-                        {/* <iframe src="https://docs.google.com/document/d/e/2PACX-1vTdFQGC_v49mdt6m37odwaYx96zND4RkAU6QJOpQF_QLRD42rYq96sfpBv5YeeoUS5IneQWR4DaQ_RL/pub?embedded=true"></iframe> */}
-                    </div>
                     <h3 style={{ marginRightt: '100%', marginTop: '15px', marginBottom: '15px'}}>Hello👋 I'M MINHAJ SADIK</h3>
                     <a href={CV} download="Resume"><button className="btn btn-success my-2 my-sm-0 btn-style">Get Resume</button></a>
                     <Link to="/skill"><button className="btn btn-outline-success my-2 my-sm-0 btn-style">My Skill</button></Link>
+                </div>
+                <div data-aos="flip-up" className="col-md-6 about-mySelf">
+                <div>
+                    <div className={styles.background} />
+
+                    <Parallax pages={5}>
+                        <ParallaxLayer offset={0} speed={0.5} style={{ ...alignCenter, justifyContent: 'center' }}>
+                        <p className={styles.scrollText}>Name: Minhaj Sadik</p>
+                        </ParallaxLayer>
+
+                        <ParallaxLayer sticky={{ start: 1, end: 3 }} style={{ ...alignCenter, justifyContent: 'flex-start' }}>
+                        <div className={`${styles.card} ${styles.sticky}`}>
+                            <p>Software Enginner</p>
+                        </div>
+                        </ParallaxLayer>
+
+                        <ParallaxLayer offset={1.5} speed={1.5} style={{ ...alignCenter, justifyContent: 'flex-end' }}>
+                        <div className={`${styles.card} ${styles.parallax} ${styles.purple}`}>
+                            <p>Front-End</p>
+                        </div>
+                        </ParallaxLayer>
+
+                        <ParallaxLayer offset={2.5} speed={1.5} style={{ ...alignCenter, justifyContent: 'flex-end' }}>
+                        <div className={`${styles.card} ${styles.parallax} ${styles.blue}`}>
+                            <p>Back-End</p>
+                        </div>
+                        </ParallaxLayer>
+                    </Parallax>
+                     
+                 </div>
+                    {/* <h3 style={{ marginRightt: '100%', marginTop: '15px', marginBottom: '15px'}}>Hello👋 I'M MINHAJ SADIK</h3>
+                    <a href={CV} download="Resume"><button className="btn btn-success my-2 my-sm-0 btn-style">Get Resume</button></a>
+                    <Link to="/skill"><button className="btn btn-outline-success my-2 my-sm-0 btn-style">My Skill</button></Link> */}
                 </div>
             </div>
         </div>
---
+++ src/component/LoveToDo/LoveToDo.js
@@ -0,0 +1,113 @@
+import Aos from "aos";
+import "aos/dist/aos.css";
+import React, { useEffect } from "react";
+import { FaMotorcycle } from "react-icons/fa";
+import { GiWorld } from "react-icons/gi";
+import { MdComputer, MdLibraryBooks } from "react-icons/md";
+import "./LoveToDo.css";
+
+const LoveToDo = () => {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
+  return (
+    <div className="container loveToDo">
+      <div className="back-line d-flex align-items-center skill-title">
+        <div className="front-box d-flex align-items-center">
+          <p className="d-flex align-items-center">LoveToDo</p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-6 col-md-3">
+          <div data-aos="flip-right" className="love-style">
+            <h3 className="blog-icon">
+              <MdLibraryBooks />
+            </h3>
+            <h3 className="blog-title">Blogging</h3>
+          </div>
+        </div>
+        <div className="col-sm-6 col-md-3">
+          <div data-aos="flip-right" className="love-style">
+            <h3 className="blog-icon">
+              <GiWorld />
+            </h3>
+            <h3 className="blog-title">Traveling</h3>
+          </div>
+        </div>
+        <div className="col-sm-6 col-md-3">
+          <div data-aos="flip-right" className="love-style">
+            <h3 className="blog-icon">
+              <MdComputer />
+            </h3>
+            <h3 className="blog-title">Computing</h3>
+          </div>
+        </div>
+        <div className="col-sm-6 col-md-3">
+          <div data-aos="flip-right" className="love-style">
+            <h3 className="blog-icon">
+              <FaMotorcycle />
+            </h3>
+            <h3 className="blog-title">Biking</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoveToDo;
+
+// import { IParallax, Parallax, ParallaxLayer } from "@react-spring/parallax";
+// import React, { useRef } from "react";
+// import styles from "./../../styles.module.css";
+
+// interface PageProps {
+//   offset: number;
+//   gradient: string;
+//   onClick: () => void;
+// }
+
+// const Page = ({ offset, gradient, onClick }: PageProps) => (
+//   <>
+//     <ParallaxLayer offset={offset} speed={0.2} onClick={onClick}>
+//       <div className={styles.slopeBegin} />
+//     </ParallaxLayer>
+
+//     <ParallaxLayer offset={offset} speed={0.6} onClick={onClick}>
+//       <div className={`${styles.slopeEnd} ${styles[gradient]}`} />
+//     </ParallaxLayer>
+
+//     <ParallaxLayer
+//       className={`${styles.text} ${styles.number}`}
+//       offset={offset}
+//       speed={0.3}
+//     >
+//       <span>0{offset + 1}</span>
+//     </ParallaxLayer>
+//   </>
+// );
+
+// export default function App() {
+//   const parallax = useRef<IParallax>(null);
+
+//   const scroll = (to: number) => {
+//     if (parallax.current) {
+//       parallax.current.scrollTo(to);
+//     }
+//   };
+//   return (
+//     <div style={{ background: "#dfdfdf" }} className="container loveToDo">
+//       <Parallax
+//         className={styles.container}
+//         ref={parallax}
+//         pages={3}
+//         horizontal
+//       >
+//         <Page offset={0} gradient="pink" onClick={() => scroll(1)} />
+//         <Page offset={1} gradient="teal" onClick={() => scroll(2)} />
+//         <Page offset={2} gradient="tomato" onClick={() => scroll(0)} />
+//       </Parallax>
+//     </div>
+//   );
+// }

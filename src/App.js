e40c43bf63e4d4ace29a885f4c316b0798982b8c
MinhajSadik <MinhajoSadik@gmail.com>
2021-10-23 12:55:12 +0600

    [done\add] i recived email from my portfolio

--- src/App.js
+++ src/App.js
@@ -1,71 +1,58 @@
-import React from 'react';
-import MessengerCustomerChat from 'react-messenger-customer-chat';
-import {
-  BrowserRouter as Router, Route, Switch
-} from "react-router-dom";
-import './App.css';
-import About from './component/About/About';
-import Contact from './component/Contact/Contact';
-import Footer from './component/Footer/Footer';
-import Header from './component/Header/Header';
-import Intro from './component/Intro/Intro';
-import LoveToDo from './component/LoveToDo/LoveToDo';
-import Projects from './component/Projects/Projects';
-import Skill from './component/Skill/Skill';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import About from "./Modules/About/About";
+import Contact from "./Modules/Contact/Contact";
+import Footer from "./Modules/Footer/Footer";
+import Header from "./Modules/Header/Header";
+import Intro from "./Modules/Intro/Intro";
+import LoveToDo from "./Modules/LoveToDo/LoveToDo";
+import Projects from "./Modules/Projects/Projects";
+import Skill from "./Modules/Skill/Skill";
 
 function App() {
-
   return (
-  <div>
-     <MessengerCustomerChat
-        pageId="<110110294633684>"
-        appId="<540562193743609>"
-        htmlRef="<REF_STRING>"
-      />
-    <Router>
-      <Header/>
-      <Switch>
-        
-        <Route exact path="/">
-          <Intro />
-          <About />
-          <Skill />
-          <LoveToDo />
-          <Projects />
-          <Contact />
-        </Route>
+    <div>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Intro />
+            <About />
+            <Skill />
+            <LoveToDo />
+            <Projects />
+            <Contact />
+          </Route>
 
-        <Route path="/intro">
-          <Intro />
-       </Route>
+          <Route path="/intro">
+            <Intro />
+          </Route>
 
-        <Route path="/about">
-          <About/>
-        </Route>
+          <Route path="/about">
+            <About />
+          </Route>
 
-        <Route path="/skill">
-          <Skill />
-          <LoveToDo/>
-        </Route>
+          <Route path="/skill">
+            <Skill />
+            <LoveToDo />
+          </Route>
 
-        <Route path="/projects">
-          <Projects/>
-        </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
 
-        <Route path="/contact">
-          <Contact/>
-        </Route>
-      
-        <Route path="*">
-          <Intro />
-        </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
 
-      </Switch>
-      <Footer/>
-    </Router>
-
-  </div>
+          <Route path="*">
+            <Intro />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
   );
 }
 

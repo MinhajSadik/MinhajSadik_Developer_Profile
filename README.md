a01f7950e9fdcf56597ba3b7b03def6c4b145021
MinhajSadik <MinhajoSadik@gmail.com>
2023-01-05 12:04:53 +0600

    [DONE] next:version installed

--- README.md
+++
@@ -1,3 +0,0 @@
-# Getting Started Portfolio Website Feature
-
-## Html, Css, JavaScript, Bootstrap, React-Bootstrap, React.js, Material-UI, React-Router, React-Form-Hook, Aos.js etc
--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
